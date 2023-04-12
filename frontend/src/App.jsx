8c86d38c689bfbb0fe8a018d9d87cc2069697eda
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-04-12 15:23:39 +0200

    jpp de git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import Home from "./pages/Home";
-
+import axios from 'axios'
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [api,setApi] = useState([])
+  const hehe= ()=>{
+  axios.get("https://data.paysdelaloire.fr/api/records/1.0/search/?dataset=234400034_070-002_offre-touristique-fetes_et_manifestations-rpdl&q=&rows=1779&facet=categorie&facet=commune&facet=periodicite&facet=tarifgratuit&facet=resaenligneouinon&facet=departement%22")
+  .then(((response) => { return response.data}))
+  .then ((data)=>{setApi(data.result);console.log(data)})
+  .catch((err) => {console.warn('attention')})
+}
+
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+      <button onClick={hehe}> azdaz</button>
     </div>
   );
 }
