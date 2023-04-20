7188b859bf7f2671cf10780a90ac80de6901014f
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-04-20 16:18:45 +0200

    ajout d'un filtre sur departement et calendrier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import "./index.scss";
 import axios from "axios";
@@ -12,59 +11,88 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Card from "./components/Card";
 import TarifGratuit from "./components/TarifGratuit";
-import Categ from "./components/Categ"
+import Categ from "./components/Categ";
 import Dept from "./components/Dept";
+import FiltreDate from "./components/FiltreDate";
 /* import _index from "./Styles/_index.scss"; */
 
-
 function App() {
-  const [dept,setDpt]=useState("All")
-  const [categ,setCateg]=useState("All")
+  const [dept, setDpt] = useState("All");
+  const [categ, setCateg] = useState("All");
   const [api, setApi] = useState(undefined);
   const [free, setFree] = useState("Payant et gratuit");
-  const [minValue, set_minValue] = useState(0);
-  const [maxValue, set_maxValue] = useState(300);
-  const handleInput = (e) =>{
-    set_minValue(e.minValue);
-	set_maxValue(e.maxValue);
-  }
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(300);
+  const [calend, setCalend] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const handleInput = (e) => {
+    setMinValue(e.minValue);
+    setMaxValue(e.maxValue);
+  };
   useEffect(() => {
     axios
       .get(
         "https://data.paysdelaloire.fr/api/records/1.0/search/?dataset=234400034_070-002_offre-touristique-fetes_et_manifestations-rpdl&q=&rows=1779&facet=categorie&facet=commune&facet=periodicite&facet=tarifgratuit&facet=resaenligneouinon&facet=departement%22"
       )
       .then((response) => {
-        setApi(response.data);console.log(response.data )
+        setApi(response.data.records);
+        console.warn(response.data.records);
       })
       .catch((err) => {
         console.warn(err);
       });
   }, []);
 
-  function handleString (str) {
-    if(str != undefined )   {  
-      if(str.match(/\|(\d+,\d+?)\|/g)==null){
-        if(str.indexOf('€')!=-1){
-          if (str.match(/(\d+?)\€/g)==null){
-            return str.match(/(\d+?) \€/g)
+  function handleString(str) {
+    if (str !== undefined) {
+      if (str.match(/\|(\d+,\d+?)\|/g) === null) {
+        if (str.indexOf("€") !== -1) {
+          if (str.match(/(\d+?)€/g) === null) {
+            return str.match(/(\d+?) €/g);
           }
-          else{return str.match(/(\d+?)\€/g)}
+          return str.match(/(\d+?)€/g);
         }
-        else{return [0]}}
-      else{
-        return  str.match(/\|(\d+,\d+?)\|/g).join('').replace(/\|/g," ").replace(/,/g,".").trim().split("  ").map(e=>Number(e))} }
-    else {
-        return [0]
-       }
-      
-      
-      
-      
-      } 
+        return [0];
+      }
+      return str
+        .match(/\|(\d+,\d+?)\|/g)
+        .join("")
+        .replace(/\|/g, " ")
+        .replace(/,/g, ".")
+        .trim()
+        .split("  ")
+        .map((e) => Number(e));
+    }
+    return [0];
+  }
+  const handleDate = (str) => {
+    let dates = [];
+    dates = str
+      .match(/(\d+\/\d+\/\d+)\|\|/g, " ")
+      .map((e) => e.replace(/\|/g, ""));
+
+    dates = dates.map((e) => {
+      const tab = e.split("");
+      tab.splice(6, 0, tab[0]);
+      tab.splice(7, 0, tab[1]);
+      tab.splice(8, 0, tab[2]);
+      tab.shift();
+      tab.shift();
+      tab.shift();
+      return tab.join("");
+    });
+
+    dates = dates.map((e) => new Date(e));
+    return dates;
+  };
 
   return (
     <div className="App">
-
       <Header />
       <Nav />
       <Categories />
@@ -72,49 +100,75 @@ function App() {
       <Contact />
       <Footer />
       <Home />
-      <p>Coucou</p>
-      <div>Valeurs : ({minValue} ; {maxValue})</div>
-      <MultiRangeSlider
-       min={0}
-       max={300}
-       step={5}
-       label={true}
-      ruler={false}
-      style={{ border: "none", boxShadow: "none", padding: "15px 10px" }}
-       minValue={minValue}
-       maxValue={maxValue}
-       barInnerColor="#153462"
-       onInput={(e) => {
-         handleInput(e);
-       }}
-      />
-      
-
       <TarifGratuit setFree={setFree} />
       {/*  condition de filtre gratuit ou payant */}
+
+      {free !== "Gratuit" && (
+        <div>
+          Valeurs : ({minValue} ; {maxValue})
+        </div>
+      )}
+      {free !== "Gratuit" && (
+        <MultiRangeSlider
+          min={0}
+          max={300}
+          step={5}
+          label
+          ruler={false}
+          style={{ border: "none", boxShadow: "none", padding: "15px 10px" }}
+          minValue={minValue}
+          maxValue={maxValue}
+          barInnerColor="#153462"
+          onInput={(e) => {
+            handleInput(e);
+          }}
+        />
+      )}
       {api && <Categ donnees={api} setCateg={setCateg} />}
-      {api && <Dept donnees={api} setDpt={setDpt}/>}
-      {}
+      {api && <Dept donnees={api} setDpt={setDpt} />}
+      {api && <FiltreDate calend={calend} setCalend={setCalend} />}
 
       {api &&
-        api.records.filter((e) => {
-          return minValue <= Math.min(...handleString(e.fields.tarifs)) && maxValue >= Math.max(...handleString(e.fields.tarifs))
-        })
-        .filter((e=>{
-          return (dept=="All" ? e : dept=="Sans Département" ? e.fields.departement == undefined : e.fields.departement==dept)
-        }))
-        .filter((e)=> {
-          return (categ=="All" ? e : categ=="Sans categorie" ? e.fields.categorie == undefined : e.fields.categorie==categ) })
-        .filter((e) => {
-          if (free === "Payant") {
-            return e.fields.tarifgratuit === "non";
-          }
-          if (free === "Gratuit") {
-            return e.fields.tarifgratuit === "oui";
-          }
+        api
+          .filter((e) => {
+            return (
+              handleDate(e.fields.ouverturegranule).filter((el) => {
+                return el <= calend[0].endDate && el >= calend[0].startDate;
+              }).length !== 0
+            );
+          })
+          .filter((e) => {
+            return (
+              minValue <= Math.min(...handleString(e.fields.tarifs)) &&
+              maxValue >= Math.max(...handleString(e.fields.tarifs))
+            );
+          })
+          .filter((e) => {
+            if (dept === "All") {
+              return e;
+            }
+            return dept === "Sans Département"
+              ? e.fields.departement === undefined
+              : e.fields.departement === dept;
+          })
+          .filter((e) => {
+            if (categ === "All") {
+              return e;
+            }
+            return categ === "Sans categorie"
+              ? e.fields.categorie === undefined
+              : e.fields.categorie === categ;
+          })
+          .filter((e) => {
+            if (free === "Payant") {
+              return e.fields.tarifgratuit === "non";
+            }
+            if (free === "Gratuit") {
+              return e.fields.tarifgratuit === "oui";
+            }
             return e;
           })
-        .map((e) => {
+          .map((e) => {
             return (
               <Card
                 event={e.fields.nomoffre}
@@ -128,7 +182,6 @@ function App() {
               />
             );
           })}
-
     </div>
   );
 }
