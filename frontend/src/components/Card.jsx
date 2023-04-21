ea072c3bb3f83b80dfc27a95fc0cb65e9f8600c7
KevinNizet <kevin.nizet@gmail.com>
2023-04-21 10:37:13 +0200

    :ambulance: Création des cartes - ajout de style - création de deux boutons
    Création des cartes - ajout de style - création de deux boutons

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { AiOutlineHeart, AiFillCalendar, AiFillHome } from "react-icons/ai";
+import {
+  MdPlace,
+  MdEuroSymbol,
+  MdCategory,
+  MdExpandMore,
+  MdExpandLess,
+} from "react-icons/md";
+import { FcLike } from "react-icons/fc";
 
 function Card({
   event,
@@ -11,17 +20,66 @@ function Card({
   departement,
   date,
 }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [moreInfos, setMoreInfos] = useState(false);
+  const handleClickFavorite = () => setIsFavorite(false);
+  const handleClickNotFavorite = () => setIsFavorite(true);
+
+  function handleMoreInfos() {
+    setMoreInfos(!moreInfos);
+  }
+
   return (
     <div className="Card">
-      <p>Evénement : {event} </p>
-      <p>Catégorie : {category} </p>
-      <p>Gratuit : {price} </p>
-      {/* //FIXME: afficher méthode de paiement seulement si gratuit = non */}
-      <p>Prix : {eventPrice} </p>
-      <p>Payement : {payment} </p>
-      <p>Adresse : {adress} </p>
-      <p>Département : {departement} </p>
-      <p>Date : {date} </p>
+      <p className="CardEvent">
+        {event
+          ? event.charAt(0).toUpperCase() + event.slice(1).toLowerCase()
+          : "Aucune information"}{" "}
+      </p>
+
+      <div id="favorite">
+        <button
+          type="button"
+          onClick={isFavorite ? handleClickFavorite : handleClickNotFavorite}
+        >
+          {isFavorite ? <FcLike size={23} /> : <AiOutlineHeart size={23} />}
+        </button>
+      </div>
+
+      <p>
+        <MdCategory /> : {category || "Aucune information"}{" "}
+      </p>
+      <p className="CardDate">
+        <AiFillCalendar /> : {date || "Aucune information"}
+      </p>
+
+      <div className="card-details">
+        <button
+          type="button"
+          id="detailsArrow"
+          onClick={handleMoreInfos}
+          className="card-details-button"
+        >
+          {" "}
+          {moreInfos ? <MdExpandMore size={23} /> : <MdExpandLess size={23} />}
+        </button>
+      </div>
+
+      {moreInfos && (
+        <>
+          <p>Gratuit : {price || "Aucune information"}</p>
+          <p>
+            <MdEuroSymbol /> : {eventPrice || "Aucune information"}{" "}
+          </p>
+          <p>Payement : {payment || "Aucune information"}</p>
+          <p>
+            <MdPlace /> : {adress || "Aucune information"}{" "}
+          </p>
+          <p>
+            <AiFillHome /> : {departement || "Aucune information"}{" "}
+          </p>
+        </>
+      )}
     </div>
   );
 }
--- frontend/src/components/Dept.jsx
+++ frontend/src/components/Dept.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from "react";
+import { PropTypes } from "prop-types";
 
-const Dept = ({donnees,setDept}) => {
-    
+function Dept({ donnees, setDept }) {
+  const handleDept = () => {
+    let depa = [];
+    depa = donnees.records.map((e) => {
+      return e.fields.departement;
+    });
 
-    const handleDept = ()=>{
-        
-        let  depa=[]
-        depa =donnees.records.map((e,i)=>{
-          
-           return (e.fields.departement)
-        } )
-        
-        let filtDepa = depa.filter(function(e,i){return depa.indexOf(e)==i})
-        filtDepa.unshift('All')
-        return filtDepa.map((e,i)=>{return(e==undefined ? <option key={i} value={undefined}>Sans Département</option> : <option key={i} value = {e}>{e}</option>)})
-    }
-    const handleDepaChange = (e) =>{
-        setDept(e.target.value)
-        
-    }
+    const filtDepa = depa.filter((e, i) => {
+      return depa.indexOf(e) === i;
+    });
+    filtDepa.unshift("All");
+    return filtDepa.map((e) => {
+      return e === undefined ? (
+        <option key={e} value={undefined}>
+          Sans Département
+        </option>
+      ) : (
+        <option key={e} value={e}>
+          {e}
+        </option>
+      );
+    });
+  };
+  const handleDepaChange = (e) => {
+    setDept(e.target.value);
+  };
 
-    return (
-        <select onChange = {(e)=>handleDepaChange(e)}>
-
-
-            {handleDept()}
-        </select>
-    );
-};
+  return <select onChange={(e) => handleDepaChange(e)}>{handleDept()}</select>;
+}
 
 export default Dept;
+Dept.propTypes = {
+  donnees: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+  ).isRequired,
+  setDept: PropTypes.string.isRequired,
+};
